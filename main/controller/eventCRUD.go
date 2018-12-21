f54954eda6828f664df4330fd993b37bc099f057
angadsharma1016 <angadsharma1016@gmail.com>
2018-12-21 05:52:05 +0530

    add an event

--- main/controller/eventCRUD.go
+++ main/controller/eventCRUD.go
@@ -8,6 +8,89 @@ import (
 	"../model"
 )
 
+/**
+ * @api {post} /event/create create a new event
+ * @apiName create a new event
+ * @apiGroup admin
+ *
+ * @apiParam {String} clubName Name of your club
+ * @apiParam {String} name Name of your event
+ * @apiParam {String} toDate ending date
+ * @apiParam {String} fromDate start date
+ * @apiParam {String} toTime start time
+ * @apiParam {String} fromTime ending time
+ * @apiParam {String} budget budget
+ * @apiParam {String} description event description
+ * @apiParam {String} [category] category of the event
+ * @apiParam {String} venue event venue
+ * @apiParam {String} [attendance] Name of your club
+ * @apiParam {String} expectedParticipants Expected Participants in the event
+ * @apiParam {Object} facultyCoordinator faculty coordinator details (Participant Object)
+ * @apiParam {Object} studentCoordinator student coordinator details (Participant Object)
+ * @apiParam {Object} guest guest details (Guest object)
+ * @apiParam {String} PROrequest PRO department requests
+ * @apiParam {String} campusEngineerRequest Campus engineer requests
+ * @apiParam {String} duration duration of event
+ * @apiParam {Object} mainSponsor sponsor details (Participant Object)
+ *
+ * @apiParamExample {json} request-example
+ *
+ * {
+   "clubName":"GDG",
+   "name":"DEVFEST",
+   "toDate":"10TH OCTOBER",
+   "fromDate":"8TH OCTOBER",
+   "toTime":"10 PM",
+   "fromTime":"11 AM",
+   "budget":"200000",
+   "description":"TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING",
+   "category":"TECHNICAL",
+   "venue":"ANNA AUDI",
+   "attendance":"4000",
+   "expectedParticipants":"4000",
+   "facultyCoordinator":{
+      "name":"NOORU MAA",
+      "registrationNumber":"17BBE1010",
+      "email":"SDADAS@A.COM",
+      "phoneNumber":"919191991911",
+      "gender":"M",
+      "eventsAttended":"ALL"
+   },
+   "studentCoordinator":{
+      "name":"NOORU BAAP",
+      "registrationNumber":"17BBE1010",
+      "email":"SDADAS@A.COM",
+      "phoneNumber":"919191991911",
+      "gender":"M",
+      "eventsAttended":"ALL"
+   },
+   "guest":{
+      "name":"ALLAHH DAAS",
+      "email":"ASDSAD#ASD.COM",
+      "phoneNumber":"11111111111",
+      "gender":"F",
+      "stake":"SOME MONAYYYY",
+      "locationOfStay":"TERA GHAR"
+   },
+   "PROrequest":"SAJDOOSIJANDFSAKFDSAFD",
+   "campusEngineerRequest":"SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD",
+   "duration":"16 hours",
+   "mainSponsor":{
+      "name":"ALLAHH DAAS",
+      "email":"ASDSAD#ASD.COM",
+      "phoneNumber":"11111111111",
+      "gender":"F",
+      "stake":"SOME MONAYYYY",
+      "locationOfStay":"TERA GHAR"
+   }
+}
+ *
+* @apiParamExample {json} response-example
+{
+	"message":true
+}
+ * @apiSuccess {Status} Boolean true or false.
+*/
 func createEvent(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -17,11 +100,19 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
-	model.CreateEvent(data)
+	if err = model.CreateEvent(data); err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(struct {
+			Status  bool   `json:"status"`
+			Message string `json:"message"`
+		}{false, "some error occurreed"})
+		return
+	}
 
 	json.NewEncoder(w).Encode(struct {
-		message string
-	}{"Done"})
+		Status  bool   `json:"status"`
+		Message string `json:"message"`
+	}{true, "new node created successfully"})
 }
 
 func eventCRUDHandler() {
--- main/model/eventCRUD.go
+++ main/model/eventCRUD.go
@@ -2,19 +2,44 @@ package model
 
 import "log"
 
-func CreateEvent(e Event) {
-	result, err := session.Run("CREATE (n:EVENT {name:$name, clubName:$clubName}) RETURN n", map[string]interface{}{
-		"name":     e.Name,
-		"clubName": e.ClubName,
-	})
-	handleErr(err)
+func CreateEvent(e Event) error {
 
-	for result.Next() {
-		log.Println(result.Record().GetByIndex(0))
+	result, err := session.Run(`CREATE (n:EVENT {name:$name, clubName:$clubName, ToDate:$toDate, 
+		FromDate: $fromDate, ToTime:$toTime, FromTime:$fromTime, Budget:$budget, 
+		Description:$description, Category:$category, Venue:$venue, Attendance:$attendance, 
+		ExpectedParticipants:$expectedParticipants, PROrequest:$PROrequest, 
+		CampusEngineerRequest:$campusEngineerRequest, Duration:$duration}) 
+		RETURN n.name`, map[string]interface{}{
+
+		"name":                  e.Name,
+		"clubName":              e.ClubName,
+		"toDate":                e.ToDate,
+		"fromDate":              e.FromDate,
+		"toTime":                e.ToTime,
+		"fromTime":              e.FromTime,
+		"budget":                e.Budget,
+		"description":           e.Description,
+		"category":              e.Category,
+		"venue":                 e.Venue,
+		"PROrequest":            e.PROrequest,
+		"campusEngineerRequest": e.CampusEngineerRequest,
+		"duration":              e.Duration,
+		"attendance":            e.Attendance,
+		"expectedParticipants":  e.ExpectedParticipants,
+	})
+	if err != nil {
+		return err
 	}
+
+	// for result.Next() {
+	// 	log.Println(result.Record().GetByIndex(1))
+	// }
+	result.Next()
+	log.Println(result.Record().GetByIndex(0).(string))
 
 	if err = result.Err(); err != nil {
-		handleErr(err)
+		return err
 	}
 
+	return nil
 }
